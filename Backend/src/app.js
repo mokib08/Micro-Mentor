94e84f88640594d51bd2c6ed1096accf842486ef
PuneetVerma07 <pk2382849@gmail.com>
2025-09-19 17:55:46 +0530

    Database connected successfully

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const cookieParser = require("cookie-parser")
 const tastRoute = require('./routes/task.Routes')
 
 const app = express();
-
+app.use(express.json())
+app.use(cookieParser())
 
 
 app.use('/api/task', tastRoute)
